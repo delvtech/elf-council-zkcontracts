3dba702dbea01bd781bc31d67c4681683fe804f3
Matthew Brown <mattfdbrown@gmail.com>
2022-03-11 11:54:26 -0800

    lint fixes

--- contracts/PrivateAirdrop.sol
+++ contracts/PrivateAirdrop.sol
@@ -2,6 +2,19 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+interface ILockingVault {
+    /// @notice Deposits and delegates voting power to an address provided with the call
+    /// @param fundedAccount The address to credit this deposit to
+    /// @param amount The amount of token which is deposited
+    /// @param firstDelegation First delegation address
+    function deposit(
+        address fundedAccount,
+        uint256 amount,
+        address firstDelegation
+    ) external;
+}
 
 interface IPlonkVerifier {
     function verifyProof(bytes memory proof, uint256[] memory pubSignals)
@@ -10,14 +23,9 @@ interface IPlonkVerifier {
         returns (bool);
 }
 
-interface IERC20 {
-    function transfer(address recipient, uint256 amount)
-        external
-        returns (bool);
-}
-
 /// @title An example airdrop contract utilizing a zk-proof of MerkleTree inclusion.
 contract PrivateAirdrop is Ownable {
+    using SafeERC20 for IERC20;
     IERC20 public airdropToken;
     uint256 public amountPerRedemption;
     IPlonkVerifier verifier;
@@ -29,22 +37,30 @@ contract PrivateAirdrop is Ownable {
     uint256 constant SNARK_FIELD =
         21888242871839275222246405745257275088548364400416034343698204186575808495617;
 
+    ILockingVault public immutable vault;
+
     constructor(
         IERC20 _airdropToken,
         uint256 _amountPerRedemption,
         IPlonkVerifier _verifier,
-        bytes32 _root
+        bytes32 _root,
+        address _vault
     ) {
         airdropToken = _airdropToken;
         amountPerRedemption = _amountPerRedemption;
         verifier = _verifier;
         root = _root;
+        vault = ILockingVault(_vault);
+        // Give maximum allowance
+        airdropToken.safeApprove(_vault, type(uint256).max);
     }
 
     /// @notice verifies the proof, collects the airdrop if valid, and prevents this proof from working again.
-    function collectAirdrop(bytes calldata proof, bytes32 nullifierHash)
-        public
-    {
+    function claimAirdropAndDelegate(
+        bytes calldata proof,
+        bytes32 nullifierHash,
+        address delegate
+    ) public {
         require(
             uint256(nullifierHash) < SNARK_FIELD,
             "Nullifier is not within the field"
@@ -61,7 +77,7 @@ contract PrivateAirdrop is Ownable {
         );
 
         nullifierSpent[nullifierHash] = true;
-        airdropToken.transfer(msg.sender, amountPerRedemption);
+        vault.deposit(msg.sender, amountPerRedemption, delegate);
     }
 
     /// @notice Allows the owner to update the root of the merkle tree.
