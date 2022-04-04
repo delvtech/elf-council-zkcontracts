0ed6e2617c85fe0a2df5134f0858525df1bceff4
Matthew Brown <mattfdbrown@gmail.com>
2022-04-04 06:33:34 -1000

    update getMockMerkleTree

--- test/merkle/getMockMerkleTree.ts
+++ test/merkle/getMockMerkleTree.ts
@@ -1,12 +1,8 @@
 import leavesJson from "test/leaves.json";
 import { MerkleTree } from "zkp-merkle-airdrop-lib/src";
-// import { MerkleTree as MT } from "merkletreejs";
 
 export function getMockMerkleTree() {
   const leaves = leavesJson.map(({ commitment }) => commitment).map(BigInt);
   const merkleTree = MerkleTree.createFromLeaves(leaves);
   return merkleTree;
-  // const otherMerkleTree = new MT(leaves);
-  // console.log("merkleTree", otherMerkleTree);
-  // return otherMerkleTree;
 }
